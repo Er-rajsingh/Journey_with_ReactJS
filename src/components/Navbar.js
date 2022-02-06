514811836cfe042303a1d7b34c59b39e31c66e8e
Er-rajsingh <raj.singh_cs19@gla.ac.in>
2022-02-07 02:31:50 +0530

    updated navbar home link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ export default function Navbar(props) {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               {/* <Link className="nav-link active" aria-current="page" to="/"> */}
-              <a className="nav-link active" aria-current="page" href="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
